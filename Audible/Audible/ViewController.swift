8e9a25791abbc8206c160e80522a4c678a4073d4
Tesleem Amuda <tesleem.amuda@gmail.com>
2021-08-22 20:48:50 +0100

    Implement skip and next button

--- Audible/Audible/ViewController.swift
+++ Audible/Audible/ViewController.swift
@@ -48,19 +48,47 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return pc
     }()
     
-    let skipButton: UIButton = {
+    lazy var skipButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Skip", for: .normal)
         button.setTitleColor(UIColor(red: 247/255, green: 154/255, blue: 27/255, alpha: 1), for: .normal)
+        button.addTarget(self, action: #selector(skip), for: .touchUpInside)
         return button
     }()
     
-    let nextButton: UIButton = {
+    @objc func skip() {
+        // we only need two lines to do this
+        pageControl.currentPage = pages.count - 1
+        nextPage()
+    }
+    
+    lazy var nextButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Next", for: .normal)
         button.setTitleColor(UIColor(red: 247/255, green: 154/255, blue: 27/255, alpha: 1), for: .normal)
+        button.addTarget(self, action: #selector(nextPage), for: .touchUpInside)
         return button
     }()
+    
+    @objc func nextPage() {
+        //we are on the last page
+        if pageControl.currentPage == pages.count {
+            return
+        }
+        
+        //second last page
+        if pageControl.currentPage == pages.count - 1 {
+            moveControlConstraintsOffScreen()
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.view.layoutIfNeeded()
+                }, completion: nil)
+        }
+        
+        let indexPath = IndexPath(item: pageControl.currentPage + 1, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        pageControl.currentPage += 1
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -104,6 +132,12 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             self.view.frame = CGRect(x: 0, y: -50, width: self.view.frame.width, height: self.view.frame.height)
             
             }, completion: nil)
+    }
+    
+    fileprivate func moveControlConstraintsOffScreen() {
+        pageControlBottomAnchor?.constant = 80
+        skipButtonTopAnchor?.constant = -80
+        nextButtonTopAnchor?.constant = -80
     }
     
     fileprivate func registerCells() {
